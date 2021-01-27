776c0a9834ae0309cc4e0f64879c10414f8a0aed
Alexander Maslennikov <amaslenn@gmail.com>
2021-01-27 15:43:52 +0100

    Unit tests for utils

--- src/pds/api/utils/environment.py
+++ src/pds/api/utils/environment.py
@@ -6,20 +6,17 @@ from pds.api.log import get_logger, debug_enabled
 from opera.commands.deploy import deploy_service_template as opera_deploy
 from opera.commands.undeploy import undeploy as opera_undeploy
 
+logger = get_logger(__name__)
 
 class DeploymentEnvironment:
 
     def __init__(self):
         self.deployments = {}
-        self.logger = get_logger(__name__)
 
     def setup(self, ssh_keys, env_vars):
         for ssh_key in ssh_keys:
             self.deployments[templates.SSH_KEY_BLUEPRINT] = \
                 self._deploy_key(ssh_key[0], ssh_key[1])
-        for env_var in env_vars:
-            self.deployments[templates.ENV_BLUEPRINT] = \
-                self._deploy_env(env_var)
 
     def cleanup(self):
         errors = []
@@ -33,7 +30,7 @@ class DeploymentEnvironment:
         return errors
 
     def _deploy_key(self, key, password):
-        self.logger.info("Deploying SSH key to SSH agent")
+        logger.info("Deploying SSH key to SSH agent")
         key_path, key_template = templates.get_service_template(
             templates.SSH_KEY_BLUEPRINT
             )
@@ -52,25 +49,8 @@ class DeploymentEnvironment:
 
         return mem_storage
 
-    def _deploy_env(self, env_vars):
-        env_path, env_template = templates.get_service_template(
-            templates.ENV_BLUEPRINT
-            )
-
-        mem_storage = SafeMemoryStorage.create()
-        inputs = {"env-var-map": env_vars}
-        os.chdir(env_path)
-        opera_deploy(
-            env_template, inputs,
-            mem_storage,
-            verbose_mode=debug_enabled(), num_workers=1,
-            delete_existing_state=True
-            )
-
-        return mem_storage
-
     def _undeploy(self, storage):
-        self.logger.info("Removing deployed prerequisite")
+        logger.info("Removing deployed prerequisite")
         opera_undeploy(
             storage, verbose_mode=debug_enabled(), num_workers=1
             )
--- src/tests/unit/api/test_utils.py
+++ src/tests/unit/api/test_utils.py
@@ -1,8 +1,37 @@
+import pytest
 import pds.api.utils.templates as templates
+import pds.api.utils.inputs as inputs
+from unittest.mock import MagicMock
+from pds.api.utils.environment import DeploymentEnvironment
 from pds.api.openapi.models.platform_type import PlatformType
 
 
 class TestUtils:
+
+    @pytest.fixture
+    def inputs_dict(self):
+        inputs = {
+                "frontend-address": "14.15.11.12",
+                "user": "alexander",
+                "_get_secret_ssh": "pds/ssh_key_slurm:pds",
+                "_ssh_password": "my_password",
+                "namespace": "TestSlurm"
+                }
+
+        return inputs
+
+    @pytest.fixture
+    def inputs_dict(self):
+        inputs = {
+                "frontend-address": "14.15.11.12",
+                "user": "alexander",
+                "_get_secret_ssh": "pds/ssh_key_slurm:pds",
+                "_ssh_password": "my_password",
+                "namespace": "TestSlurm",
+                "_storage_key": "TEST_STORAGE_KEY"
+                }
+
+        return inputs
 
     def test_templates(self, flask_app):
         with flask_app.app.app_context():
@@ -13,4 +42,36 @@ class TestUtils:
             bp_name = templates.get_service_template(PlatformType.OPENSTACK)
             assert bp_name[1] == "openstack_info.yaml"
             bp_name = templates.get_service_template(PlatformType.TORQUE)
-            assert bp_name[1] == "torque_wm_info.yaml"            
+            assert bp_name[1] == "torque_wm_info.yaml"
+
+    def test_environment_setup(self, mocker, flask_app):
+        with flask_app.app.app_context():
+            mocker.patch("pds.api.utils.environment.opera_deploy")
+            mocker.patch("pds.api.utils.environment.opera_undeploy")
+            ssh_keys = [("TEST_KEY", "TEST_PASS")]
+            env_vars = ["TEST_VAR"]
+            env = DeploymentEnvironment()
+            env.setup(ssh_keys, env_vars)
+            assert len(env.deployments) == 1
+            env.cleanup()
+            assert len(env.deployments) == 1
+
+    def test_preprocess(self, mocker, flask_app, inputs_dict):
+        def get_json():
+            return {
+                        "data":
+                        {
+                            "_ssh_key": "test"
+                        }
+                    }
+        with flask_app.app.app_context():
+            get_response = MagicMock()
+            get_response.json = get_json
+            mocker.patch("pds.api.utils.inputs.session.get",
+                         return_value=get_response)
+            mocker.patch("pds.api.utils.inputs.session.post")
+            result = inputs.preprocess_inputs(inputs_dict, "ACCESS_TOKEN")
+            assert len(result[0]) == 3
+            assert len(result[2]) == 1
+            assert result[2][0][0] == "test"
+            assert result[2][0][1] == inputs_dict["_ssh_password"]
