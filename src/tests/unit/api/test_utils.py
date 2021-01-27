c174bb3e67b31754d65a5855d6ae6e9f8f7d2833
Alexander Maslennikov <amaslenn@gmail.com>
2021-01-27 16:20:09 +0100

    Extended tests

--- src/tests/unit/api/test_utils.py
+++ src/tests/unit/api/test_utils.py
@@ -7,21 +7,8 @@ from pds.api.openapi.models.platform_type import PlatformType
 
 
 class TestUtils:
-
     @pytest.fixture
-    def inputs_dict(self):
-        inputs = {
-                "frontend-address": "14.15.11.12",
-                "user": "alexander",
-                "_get_secret_ssh": "pds/ssh_key_slurm:pds",
-                "_ssh_password": "my_password",
-                "namespace": "TestSlurm"
-                }
-
-        return inputs
-
-    @pytest.fixture
-    def inputs_dict(self):
+    def inputs_dict_full(self):
         inputs = {
                 "frontend-address": "14.15.11.12",
                 "user": "alexander",
@@ -32,6 +19,28 @@ class TestUtils:
                 }
 
         return inputs
+
+    @pytest.fixture
+    def inputs_dict_ssh(self):
+        inputs = {
+                "frontend-address": "14.15.11.12",
+                "user": "alexander",
+                "_get_secret_ssh": "pds/ssh_key_slurm:pds",
+                "_ssh_password": "my_password",
+                "namespace": "TestSlurm"
+                }
+
+        return inputs        
+
+    @pytest.fixture
+    def inputs_dict_simple(self):
+        inputs = {
+                "frontend-address": "14.15.11.12",
+                "user": "alexander",
+                "namespace": "TestSlurm"
+                }
+
+        return inputs         
 
     def test_templates(self, flask_app):
         with flask_app.app.app_context():
@@ -48,6 +57,7 @@ class TestUtils:
         with flask_app.app.app_context():
             mocker.patch("pds.api.utils.environment.opera_deploy")
             mocker.patch("pds.api.utils.environment.opera_undeploy")
+            mocker.patch("os.chdir")
             ssh_keys = [("TEST_KEY", "TEST_PASS")]
             env_vars = ["TEST_VAR"]
             env = DeploymentEnvironment()
@@ -56,7 +66,7 @@ class TestUtils:
             env.cleanup()
             assert len(env.deployments) == 1
 
-    def test_preprocess(self, mocker, flask_app, inputs_dict):
+    def test_preprocess_both_keys(self, mocker, flask_app, inputs_dict_full):
         def get_json():
             return {
                         "data":
@@ -70,8 +80,35 @@ class TestUtils:
             mocker.patch("pds.api.utils.inputs.session.get",
                          return_value=get_response)
             mocker.patch("pds.api.utils.inputs.session.post")
-            result = inputs.preprocess_inputs(inputs_dict, "ACCESS_TOKEN")
+            result = inputs.preprocess_inputs(inputs_dict_full, "ACCESS_TOKEN")
             assert len(result[0]) == 3
             assert len(result[2]) == 1
             assert result[2][0][0] == "test"
-            assert result[2][0][1] == inputs_dict["_ssh_password"]
+            assert result[2][0][1] == inputs_dict_full["_ssh_password"]
+
+    def test_preprocess_both_keys(self, mocker, flask_app, inputs_dict_ssh):
+        def get_json():
+            return {
+                        "data":
+                        {
+                            "_ssh_key": "test"
+                        }
+                    }
+        with flask_app.app.app_context():
+            get_response = MagicMock()
+            get_response.json = get_json
+            mocker.patch("pds.api.utils.inputs.session.get",
+                         return_value=get_response)
+            mocker.patch("pds.api.utils.inputs.session.post")
+            result = inputs.preprocess_inputs(inputs_dict_ssh, "ACCESS_TOKEN")
+            assert len(result[0]) == 3
+            assert len(result[2]) == 1
+            assert result[2][0][0] == "test"
+            assert result[2][0][1] == inputs_dict_ssh["_ssh_password"]
+
+    def test_preprocess_simple(self, mocker, flask_app, inputs_dict_simple):
+        with flask_app.app.app_context():
+            result = inputs.preprocess_inputs(inputs_dict_simple, "")
+            assert len(result[0]) == 3
+            assert len(result[2]) == 0
+    
