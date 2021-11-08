4225d8878d411a213c2a1bf51f0b07352be6462c
Alexander Maslennikov <amaslenn@gmail.com>
2021-11-08 16:34:20 +0100

    Fast disk mapping for HPC discovery

--- rebuild.sh
+++ rebuild.sh
@@ -1,9 +1,9 @@
-cd src/ansible_collections/sodalite/discovery/
-ansible-galaxy collection build --force
-cd ../../../..
-ansible-galaxy collection install src/ansible_collections/sodalite/discovery/sodalite-discovery-0.1.0.tar.gz --force
+# cd src/ansible_collections/sodalite/discovery/
+# ansible-galaxy collection build --force
+# cd ../../../..
+# ansible-galaxy collection install src/ansible_collections/sodalite/discovery/sodalite-discovery-0.1.0.tar.gz --force
 
 cd src/ansible_collections/sodalite/hpc/
 ansible-galaxy collection build --force
 cd ../../../..
-ansible-galaxy collection install src/ansible_collections/sodalite/hpc/sodalite-hpc-0.1.0.tar.gz --force
+ansible-galaxy collection install src/ansible_collections/sodalite/hpc/sodalite-hpc-1.0.0.tar.gz --force
